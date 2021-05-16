41eecff92403a2670e70d39b355385322ac3c5b6
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2021-05-16 15:12:10 -0400

    week1

---
+++ week1/NodeMBandP/app.js
@@ -0,0 +1,7 @@
+const mathCha = require('./math');
+
+console.log(mathCha);
+console.log(mathCha.sum(25, 5));
+console.log(mathCha.subtract(25, 5));
+console.log(mathCha.multiply(25, 5)); 
+console.log(mathCha.divide(25, 5));
