1a53d8f11f18385d388311166e337472607252e8
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2021-05-30 13:22:59 -0400

    week3

--- week3/BountyHunter/index.js
+++ week3/BountyHunter/index.js
@@ -3,7 +3,19 @@ var app = express();
 
 var bountyHunter = [
 
-    {"BountyRoute": 'FirstName','Jacob' 'LastName':'Brown', }
+    { 'firstName':'Jacob', 'lastName':'Brown', 'living':'yes', 'bountyAmount': 150000, 'type':'Sith', 'id':'' }
+]
+
+[
+    { 'firstName':'Kyle', 'lastName':'Thomas', 'living':'no', 'bountyAmount': 90000, 'type':'Jedi', 'id':'' }
+]
+
+[
+    { 'firstName':'Alice', 'lastName':'Bernard', 'living':'yes', 'bountyAmount': 600000, 'type':'Sith', 'id':'' }
+]
+
+[
+    { 'firstName':'David', 'lastName':'Juda', 'living':'yes', 'bountyAmount': 350000, 'type':'Jedi', 'id':'' }   
 ]
 
 app.get ('/BountyHunter', function(req, res) {
