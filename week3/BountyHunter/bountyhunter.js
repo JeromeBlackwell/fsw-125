663f913e04dd593da36582fa3e0737c7c56482fd
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2021-06-06 02:57:24 -0400

    week4

--- week3/BountyHunter/bountyhunter.js
+++ week3/BountyHunter/bountyhunter.js
@@ -8,4 +8,18 @@ router.post ('/', function (req, res) {
     res.send ( 'Post route on bountyhunter.');
 });
 
+.get('/:id', (req, res) => {
+    const bountyId =req.params.id;
+    const hunter = bountys.find(bounty => bounty._id === bountyId);
+
+res.send(hunter)
+
+.delete('/:id', (req, res) => {
+    const bountyId= req.params.id;
+    const type = bountys.type(bounty =>bounty._id === bountyId);
+    bountys.splice(type, 1);
+
+    res.send('Resource successfully deleted!')
+})
+
 module.exports = router;
